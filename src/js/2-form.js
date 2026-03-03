b3d4310c2072d5fac1563cdcedcfc59719f63c85
Viktor Tar <tarviky@gmail.com>
2026-03-03 10:52:09 +0200

    ht-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,38 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', evt => {
+  const name = evt.target.name;
+  const value = evt.target.value;
+  if (name in formData) {
+    formData[name] = value.trim();
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
